553c2d7b251bf3b0055de474f6f504b8567265b4
kritebh <kritebhlaganbibhakar@gmail.com>
2021-03-04 23:50:53 +0530

    Guess the number updated

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,11 +1,12 @@
 # Guess the number game
 import random
-def guessgame(x):
+def mygame(x):
     mysterious_number = random.randint(1,x)
     # print(mysterious_number)
     entered_number = 0
     counter = 0
     name = input('Your Name : ')
+    #loop to check until user guess the correct number
     while entered_number!=mysterious_number:
         entered_number = int(input(f'Guess the number between 1 and {x} : '))
         if entered_number <mysterious_number:
@@ -13,12 +14,15 @@ def guessgame(x):
         elif entered_number>mysterious_number:
             print('Guess low')
         counter = counter+1
+    #condition to print different messages
     if counter <=5:
-        print(f'Congratulations,{name} have guess the number in just {counter} round')
+        print(f"Congratulations,{name} have guess the number in just {counter} round, That's amazing")
     elif counter >=6 and counter <=10:
         print(f'{name},You have guess the number in {counter} round')
     elif counter >=11 and counter <=15:
         print(f'{name}, You have guess the number in {counter} round, Good luck next time')
     else:
         print(mysterious_number)
-guessgame(20)
+
+#calling the function
+mygame(20)
